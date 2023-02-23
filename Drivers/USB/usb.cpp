9250be20d864e2f8c1ea330f341de1a2f8171784
koteiko_vv <koteiko_vv@peleng.jsc.local>
2023-02-23 16:38:28 +0300

    update USB

--- Drivers/USB/usb.cpp
+++ Drivers/USB/usb.cpp
@@ -8,7 +8,148 @@ namespace drivers::usb {
     Usb::Usb(drivers::ClockControl &clockControl1) : clockControl(clockControl1) {}
 
     void Usb::Init() noexcept {
+
+
+        MspInit();
+
+        libs::MWR::write_register(GAHBCFG, ~0x00000001); // Disable the Interrupts
+
+        //-------------------CoreInit--------------------------
+
+        libs::MWR::enableNumberBit(GUSBCFG, 6);
+
+        CoreReset();
+
+        libs::MWR::enableNumberBit(GCCFG,16);
+
+        //-------------------Force Device Mode------------------
+
+        libs::MWR::write_register(GUSBCFG,~0x60000000);
+        libs::MWR::enableNumberBit(GUSBCFG,30);
+
+        while (libs::MWR::readBit<std::uint32_t>(GINTSTS,0))
+        {
+            clockControl.mDelay(1);
+        }
+
+        //-------------------Init Device------------------------
+
+        for (std::size_t i = 0U; i < 15U; i++)
+        {
+           libs::MWR::write_register(DIEPTXF + (i * 4) ,0);
+        }
+
+        libs::MWR::enableNumberBit(DCTL,1);
+        libs::MWR::write_register(GCCFG,~0x80000);
+        libs::MWR::write_register(GCCFG,~0x80000);
+        libs::MWR::write_register(DCFG,0);
+
+        //-------------------Core Full speed--------------------
+
+        libs::MWR::write_register(DCFG,3);
+
+        //-------------------Flush the FIFO--------------------
+
+        while(!libs::MWR::readBit<std::uint32_t>(GRSTCTL,31))
+        {
+
+        }
+
+        libs::MWR::write_register(GRSTCTL,32 | (2 << 6));
+
+        while(libs::MWR::readBit<std::uint32_t>(GRSTCTL,5))
+        {
+
+        }
+
+        while(!libs::MWR::readBit<std::uint32_t>(GRSTCTL,31))
+        {
+
+        }
+
+        libs::MWR::write_register(GRSTCTL, 0x10);
+
+        while(libs::MWR::readBit<std::uint32_t>(GRSTCTL,4))
+        {
+
+        }
+
+        /* Clear all pending Device Interrupts */
+
+        libs::MWR::write_register(DIEPMSK, 0);
+        libs::MWR::write_register(DOEPMSK, 0);
+        libs::MWR::write_register(DAINTMSK,0);
+
+
+        for (std::size_t i = 0U; i < 4; i++)
+        {
+            if (libs::MWR::readBit<std::uint32_t>(DIEPCTL,31))
+            {
+                if (i == 0U)
+                {
+                    libs::MWR::write_register(DIEPCTL,(1 << 27));
+                }
+                else
+                {
+                    libs::MWR::write_register(DIEPCTL,(1 << 30) | (1 << 27));
+                }
+            }
+            else
+            {
+                libs::MWR::write_register(DIEPCTL + (i * 0x20),0);
+            }
+            libs::MWR::write_register(DIEPTSIZ + (i * 0x20),0);
+            libs::MWR::write_register(DIEPINT + (i * 0x20),0xFB7FU);
+        }
+
+        for (std::size_t i = 0U; i < 4; i++)
+        {
+            if (libs::MWR::readBit<std::uint32_t>(DOEPCTL,31))
+            {
+                if (i == 0U)
+                {
+                    libs::MWR::write_register(DOEPCTL,(1 << 27));
+                }
+                else
+                {
+                    libs::MWR::write_register(DOEPCTL,(1 << 30) | (1 << 27));
+                }
+            }
+            else
+            {
+                libs::MWR::write_register(DOEPCTL + (i * 0x20),0);
+            }
+
+            libs::MWR::write_register(DOEPCTL + (i * 0x20),0);
+            libs::MWR::write_register(DOEPCTL + (i * 0x20),0xFB7FU);
+        }
+
+        libs::MWR::write_register(DIEPMSK,~(0x100));
+        libs::MWR::write_register(GINTMSK, 0);           // Disable all interrupts
+        libs::MWR::write_register(GINTSTS, 0xBFFFFFFFU); // Clear any pending interrupts
+
+        libs::MWR::setBit(GINTMSK,1 << 4);
+        libs::MWR::setBit(GINTMSK,(1 << 11) | (1 << 12) | (1 << 13) | (1 << 18) | (1 << 19) | (1 << 20) | (1 << 21) | (1 << 31));
+        libs::MWR::setBit(DCTL, 2);
+    }
+
+    void Usb::CoreReset() noexcept {
+
+        do {
+
+        } while (!libs::MWR::readBit<std::uint32_t>(GRSTCTL,31));
+
+        libs::MWR::enableNumberBit(GRSTCTL,0);
+
+        do {
+
+        } while (libs::MWR::readBit<std::uint32_t>(GRSTCTL,0));
+
+    }
+
+    void Usb::MspInit() {
         drivers::port::GPIO<drivers::port::PORT_A> gpio(clockControl);
+        drivers::nvic::NVIC nvic(clockControl);
 
         gpio.SetPinMode(gpio.PIN_12,gpio.ALTERNATE_FUNCT);
         gpio.SetPinMode(gpio.PIN_11,gpio.ALTERNATE_FUNCT);
@@ -18,7 +159,10 @@ namespace drivers::usb {
         gpio.SetAFPin(gpio.PIN_11,gpio.AF10);
 
         clockControl.module_enable(ClockControl::USB_FS_MODULE);
-
+        nvic.NVIC_Enable(nvic.OTG_FS_IRQ);
     }
+
+
+
 
 }
--- Drivers/USB/usb.h
+++ Drivers/USB/usb.h
@@ -19,19 +19,61 @@ namespace drivers::usb {
 
         enum Register : std::uintptr_t
         {
-            SR    = baseAddress,        // USART Status register,                   Address offset: 0x00
-            DR    = baseAddress + 0x04, // USART Data register,                     Address offset: 0x04
-            BRR   = baseAddress + 0x08, // USART Baud rate register,                Address offset: 0x08
-            CR1   = baseAddress + 0x0C, // USART Control register 1,                Address offset: 0x0C
-            CR2   = baseAddress + 0x10, // USART Control register 2,                Address offset: 0x10
-            CR3   = baseAddress + 0x14, // USART Control register 3,                Address offset: 0x14
-            GTPR  = baseAddress + 0x18  // USART Guard time and prescaler register, Address offset: 0x18
+          GOTGCTL            = baseAddress + 0x04, /*!< USB_OTG Control and Status Register          000h */
+          GOTGINT            = baseAddress + 0x04, /*!< USB_OTG Interrupt Register                   004h */
+          GAHBCFG            = baseAddress + 0x08, /*!< Core AHB Configuration Register              008h */
+          GUSBCFG            = baseAddress + 0x0C, /*!< Core USB Configuration Register              00Ch */
+          GRSTCTL            = baseAddress + 0x10, /*!< Core Reset Register                          010h */
+          GINTSTS            = baseAddress + 0x14, /*!< Core Interrupt Register                      014h */
+          GINTMSK            = baseAddress + 0x18, /*!< Core Interrupt Mask Register                 018h */
+          GRXSTSR            = baseAddress + 0x1C, /*!< Receive Sts Q Read Register                  01Ch */
+          GRXSTSP            = baseAddress + 0x20, /*!< Receive Sts Q Read & POP Register            020h */
+          GRXFSIZ            = baseAddress + 0x24, /*!< Receive FIFO Size Register                   024h */
+          DIEPTXF0_HNPTXFSIZ = baseAddress + 0x28, /*!< EP0 / Non Periodic Tx FIFO Size Register     028h */
+          HNPTXSTS           = baseAddress + 0x2C, /*!< Non Periodic Tx FIFO/Queue Sts reg           02Ch */
+          GCCFG              = baseAddress + 0x38, /*!< General Purpose IO Register                  038h */
+          CID                = baseAddress + 0x3C, /*!< User ID Register                             03Ch */
+          HPTXFSIZ           = baseAddress + 0x100,/*!< Host Periodic Tx FIFO Size Reg               100h */
+          DIEPTXF            = baseAddress + 0x104,/*!< dev Periodic Transmit FIFO                        */
+
+          DCFG               = baseAddress + 0x800, /*!< dev Configuration Register   800h */
+          DCTL               = baseAddress + 0x804, /*!< dev Control Register         804h */
+          DSTS               = baseAddress + 0x808, /*!< dev Status Register (RO)     808h */
+          DIEPMSK            = baseAddress + 0x810, /*!< dev IN Endpoint Mask         810h */
+          DOEPMSK            = baseAddress + 0x814, /*!< dev OUT Endpoint Mask        814h */
+          DAINT              = baseAddress + 0x818, /*!< dev All Endpoints Itr Reg    818h */
+          DAINTMSK           = baseAddress + 0x81C, /*!< dev All Endpoints Itr Mask   81Ch */
+          DVBUSDIS           = baseAddress + 0x828, /*!< dev VBUS discharge Register  828h */
+          DVBUSPULSE         = baseAddress + 0x82C, /*!< dev VBUS Pulse Register      82Ch */
+          DTHRCTL            = baseAddress + 0x830, /*!< dev threshold                830h */
+          DIEPEMPMSK         = baseAddress + 0x834, /*!< dev empty msk                834h */
+          DEACHINT           = baseAddress + 0x838, /*!< dedicated EP interrupt       838h */
+          DEACHMSK           = baseAddress + 0x83C, /*!< dedicated EP msk             83Ch */
+          DINEP1MSK          = baseAddress + 0x844, /*!< dedicated EP mask            844h */
+          DOUTEP1MSK         = baseAddress + 0x848, /*!< dedicated EP msk                  */
+
+          DOEPCTL           = baseAddress + 0xB00,  /*!< dev OUT Endpoint Control Reg           B00h + (ep_num * 20h) + 00h */
+          DOEPINT           = baseAddress + 0xB08,  /*!< dev OUT Endpoint Itr Reg               B00h + (ep_num * 20h) + 08h */
+          DOEPTSIZ          = baseAddress + 0xB10,  /*!< dev OUT Endpoint Txfer Size            B00h + (ep_num * 20h) + 10h */
+          DOEPDMA           = baseAddress + 0xB14,  /*!< dev OUT Endpoint DMA Address           B00h + (ep_num * 20h) + 14h */
+
+          DIEPCTL            = baseAddress + 0x900, /*!< dev IN Endpoint Control Reg 900h + (ep_num * 20h) + 00h */
+          DIEPINT            = baseAddress + 0x908, /*!< dev IN Endpoint Itr Reg     900h + (ep_num * 20h) + 08h */
+          DIEPTSIZ           = baseAddress + 0x910, /*!< IN Endpoint Txfer Size      900h + (ep_num * 20h) + 10h */
+          DIEPDMA            = baseAddress + 0x914, /*!< IN Endpoint DMA Address Reg 900h + (ep_num * 20h) + 14h */
+          DTXFSTS            = baseAddress + 0x918, /*!< IN Endpoint Tx FIFO Status Reg 900h + (ep_num * 20h) + 18h */
+
         };
+
+        void MspInit();
 
     public:
         Usb(ClockControl &clockControl1);
 
         void Init() noexcept;
+
+        void CoreReset() noexcept;
+
     };
 }
 
