e57fc9d7ece3a4555ae81dfe7e4b73f5084d5a7b
Комяк Андрей Павлович <komiak_ap@peleng.jsc.local>
2023-11-15 10:46:53 +0300

    add preset

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_SYSTEM_NAME "Generic")
 
-find_path(PATH_ARM_GCC arm-none-eabi-gcc)
-
 if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
     set(EXTENSION_FILE .exe)
+    find_path(PATH_ARM_GCC arm-none-eabi-gcc.exe)
 elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
     set(EXTENSION_FILE )
+    find_path(PATH_ARM_GCC arm-none-eabi-gcc)
 else()
     message(FATAL_ERROR "Unknown system")
 endif()
@@ -14,6 +14,8 @@ find_path(PATH_ARM_GCC arm-none-eabi-gcc$(EXTENSION_FILE))
 
 message("==============")
 message("PATH_ARM_GCC ${PATH_ARM_GCC}" )
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 if(${PATH_ARM_GCC} EQUAL PATH_ARM_GCC-NOTFOUND)
     message(FATAL_ERROR "Toolchain not found")
@@ -27,6 +29,6 @@ else()
     set(CMAKE_OBJCOPY ${PATH_ARM_GCC}/arm-none-eabi-objcopy${EXTENSION_FILE})
     set(CMAKE_OBJDUMP ${PATH_ARM_GCC}/arm-none-eabi-objdump${EXTENSION_FILE})
     set(SIZE ${PATH_ARM_GCC}/arm-none-eabi-size${EXTENSION_FILE})
-    set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+    
 endif()
 
